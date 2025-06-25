c17a40277f0405d9ff00586d6d6b7fabb535ce28
TorLindstrom <tor.lindstrom@winlas.se>
2025-06-25 17:05:45 +0200

    test: ordering of all clothed cards

--- Test/KataTests.cs
+++ Test/KataTests.cs
@@ -4,14 +4,16 @@ namespace Test;
 
 public class KataTests
 {
-    [Fact(DisplayName = "Pick Highest Card if there is no hand")]
-    public void PickHighestCard_WhenNoHand()
+    [Theory(DisplayName = "Pick Highest Card if there is no hand")]
+    [InlineData(new[] {"8♠", "6♦"}, new[] {"J♣", "5♥", "10♥", "2♥", "3♦"}, new [] { "J", "10", "8", "6", "5" })]
+    [InlineData(new[] {"Q♠", "A♦"}, new[] {"J♣", "K♥", "10♥", "2♥", "3♦"}, new [] { "A", "K", "Q", "J", "10" })]
+    public void PickHighestCard_WhenNoHand(string[] holeCards, string[] communityCards, string[] expected)
     {
         // Act
-        var (type, ranks) = Kata.Hand(["8♠", "6♦"], ["J♣", "5♥", "10♥", "2♥", "3♦"]);
+        var (type, ranks) = Kata.Hand(holeCards, communityCards);
         
         // Assert
         Assert.Equal("nothing", type);
-        Assert.Equal(new [] { "J", "10", "8", "6", "5" }, ranks);
+        Assert.Equal(expected, ranks);
     }
 }
