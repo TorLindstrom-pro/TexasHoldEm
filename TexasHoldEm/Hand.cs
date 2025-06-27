167b9ad5b83589b2ec564d7361467484145906f4
TorLindstrom <tor.lindstrom@winlas.se>
2025-06-27 16:12:23 +0200

    refactor: move Hand class to own file

---
+++ TexasHoldEm/Hand.cs
@@ -0,0 +1,9 @@
+﻿namespace TexasHoldEm;
+
+public class Hand(
+	Func<IEnumerable<Card>, bool> matching,
+	Func<IEnumerable<Card>, (string type, string[] ranks)> getHand)
+{
+	public Func<IEnumerable<Card>, bool> Matching { get; } = matching;
+	public Func<IEnumerable<Card>, (string type, string[] ranks)> GetHand { get; } = getHand;
+}
--- TexasHoldEm/Kata.cs
+++ TexasHoldEm/Kata.cs
@@ -41,11 +41,3 @@ public static class Kata
 		Nothing
 	];
 }
-
-public class Hand(
-	Func<IEnumerable<Card>, bool> matching,
-	Func<IEnumerable<Card>, (string type, string[] ranks)> getHand)
-{
-	public Func<IEnumerable<Card>, bool> Matching { get; } = matching;
-	public Func<IEnumerable<Card>, (string type, string[] ranks)> GetHand { get; } = getHand;
-}
