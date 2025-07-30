afb20e6fafb3bd8541a7ccee3f5a07f2985fc4d8
TorLindstrom <tor.lindstrom@winlas.se>
2025-07-30 09:04:44 +0200

    fix: two pairs can match with three pairs as well

--- TexasHoldEm/Kata.cs
+++ TexasHoldEm/Kata.cs
@@ -231,7 +231,7 @@ public class TwoPair : Hand
 	public override bool Matching(IEnumerable<Card> cards) => cards
 		.Select(card => card.Value)
 		.GroupBy(card => card)
-		.Count(group => group.Count() == 2) == 2;
+		.Count(group => group.Count() == 2) >= 2;
 
 	public override (string type, string[] ranks) GetHand(IEnumerable<Card> cards) => (
 		"two pair",
